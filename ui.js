e8765946b3a836dde206fd58688edb7b4697b7a3
pmarkiewicz <piotr.markiewicz@gmail.com>
2017-07-27 15:07:55 +0200

    for normalization reference is point at 0, 0 if one is last

--- ui.js
+++ ui.js
@@ -52,7 +52,12 @@ const parseData = function() {
 };
 
 const normalizePoints = function(points) {
-    const refZ = points[0].z;
+    let refZ = points[0].z;
+    const lastPt = points[points.length -1];
+
+    if (lastPt.x === 0.0 && lastPt.y === 0.0) {
+      refZ = lastPt.z;
+    }
 
     return points.reduce( (lst, pt) => {
       lst.push({x: pt.x, y: pt.y, z: pt.z - refZ});
